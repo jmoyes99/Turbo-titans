a75ad679bbf0224a462c3ed96307a89383095d82
jmoyes99 <j.m.moyes@student.saintkentigern.com>
2023-04-26 11:25:38 +1200

    Final version

--- main.py
+++ main.py
@@ -5,35 +5,39 @@ import time
 
 pygame.init()
 
-# Set name and variables
+# Set Name and Variables
 pygame.display.set_caption("Turbo Titans")
 SPEED = 5
-BACKGROUND_SPEED = 5
+BACKGROUND_SPEED = 8
 SCORE = 0
 
-# Set screen info
+# Set Screen info
 SCREEN_WIDTH, SCREEN_HEIGHT = 900, 650
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 FPS = 60
 FramePerSec = pygame.time.Clock()
 
-# Defining colours
+# Defining Colours
 RED = (255, 0, 0)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+BLUE = (0, 100, 255)
+GREEN = (50, 255, 50)
 
-# Making fonts
+# Making Fonts
 font = pygame.font.SysFont("Verdana", 60)
-font_small = pygame.font.SysFont("Verdana", 20)
+font_small = pygame.font.SysFont("Verdana", 50)
 game_over = font.render("Game Over", True, BLACK)
 
 background = pygame.image.load("Road final.png")
+Enemy_image = pygame.image.load("Enemy.png")
 
 
-class Hugh(pygame.sprite.Sprite):
+# Enemy Class
+class Enemy(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load("Enemy.png")
+        self.image = Enemy_image
         self.rect = self.image.get_rect()
         self.rect.center = (random.randint(250, SCREEN_WIDTH - 250), 0)
 
@@ -41,14 +45,17 @@ class Hugh(pygame.sprite.Sprite):
         global SCORE
         self.rect.move_ip(0, SPEED + 0.5)
         if (self.rect.bottom > 750):
-            SCORE += 1
-            self.rect.top = -50
-            self.rect.center = (random.randint(250, SCREEN_WIDTH - 250), 0)
+            SCORE += 1  # Adds Score
+            self.rect.top = -50  # Resets Enemy 50 pixels above the Screen
+            self.rect.center = (
+                random.randint(250, SCREEN_WIDTH - 250), 0)  # Chooses Random point on the Road to spawn in
 
+    # Blits Enemy Sprite onto Screen
     def draw(self, surface):
         surface.blit(self.image, self.rect)
 
 
+# Playable Character Class
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
@@ -57,7 +64,9 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = (450, 540)
 
+    # Moves player according to keys pressed
     def move(self):
+        # Allows player to move from left to right
         pressed_keys = pygame.key.get_pressed()
         if self.rect.left > 250:
             if pressed_keys[K_LEFT]:
@@ -65,9 +74,21 @@ class Player(pygame.sprite.Sprite):
         if self.rect.right < SCREEN_WIDTH - 250:
             if pressed_keys[K_RIGHT]:
                 self.rect.move_ip(25, 0)
+        # Cheat button that allows player to go off the road
+        if pressed_keys[K_ESCAPE]:
+            global SCORE
+            SCORE += 10
+            if pressed_keys[K_LEFT]:
+                self.rect.move_ip(-25, 0)
+            if pressed_keys[K_RIGHT]:
+                self.rect.move_ip(25, 0)
+
+    # Blits player
 
     def draw(self, surface):
         surface.blit(self.image, self.rect)
+
+    # Class for the Background
 
 
 class Background():
@@ -81,8 +102,15 @@ class Background():
         self.bgY2 = self.rectBGimg.height
         self.bgX2 = 0
 
-        self.movingDownSpeed = BACKGROUND_SPEED + 1
+        pressed_keys = pygame.key.get_pressed()
+        global BACKGROUND_SPEED
 
+        if SCORE < 15:
+            BACKGROUND_SPEED += 5
+
+        self.movingDownSpeed = BACKGROUND_SPEED
+
+    # Moves background according to the above variables
     def update(self):
         self.bgY1 += self.movingDownSpeed
         self.bgY2 += self.movingDownSpeed
@@ -91,24 +119,25 @@ class Background():
         if self.bgY2 >= self.rectBGimg.height:
             self.bgY2 = -self.rectBGimg.height
 
+    # Puts Background onto Screen
     def render(self):
         SCREEN.blit(self.bgimage, (self.bgX1, self.bgY1))
         SCREEN.blit(self.bgimage, (self.bgX2, self.bgY2))
 
 
-P1 = Player()
-E1 = Hugh()
+P = Player()
+E = Enemy()
 
 back_ground = Background()
 
 # Creating Sprites Groups
 enemies = pygame.sprite.Group()
-enemies.add(E1)
+enemies.add(E)
 all_sprites = pygame.sprite.Group()
-all_sprites.add(P1)
-all_sprites.add(E1)
+all_sprites.add(P)
+all_sprites.add(E)
 
-# Adding a new User event
+# Adding a New User event
 INC_SPEED = pygame.USEREVENT + 1
 pygame.time.set_timer(INC_SPEED, 1000)
 
@@ -133,14 +162,14 @@ while True:
         SCREEN.blit(entity.image, entity.rect)
         entity.move()
 
-    # If player and enemy collides the screen turns red and the game ends
-    if pygame.sprite.spritecollideany(P1, enemies):
-        SCREEN.fill(RED)
-        SCREEN.blit(game_over, (30, 250))
+    # If player and enemy collides the screen turns blue and the game ends
+    if pygame.sprite.spritecollideany(P, enemies):
+        SCREEN.fill(BLUE)
+        SCREEN.blit(game_over, (275, 280))
         pygame.display.update()
         for entity in all_sprites:
             entity.kill()
-        time.sleep(0.5)
+        time.sleep(1)
         pygame.quit()
 
     pygame.display.update()
